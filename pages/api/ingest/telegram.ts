22b9c7b80e344e80024bf19de0ca05fd245b75a9
Morgan Gallant <morgan@morgangallant.com>
2022-02-20 19:55:03 -0500

    echo handler
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- pages/api/ingest/telegram.ts
+++ pages/api/ingest/telegram.ts
@@ -1,12 +1,45 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-/*
-    Ingests and processes incoming Telegram messages.
-*/
+// TelegramMessage is the type of the Telegram message object.
+type TelegramMessage = {
+  message: {
+    text: string;
+    chat: {
+      id: number;
+    };
+    from: {
+      username: string;
+    };
+  };
+};
+
+// Ingests and processes incoming Telegram messages.
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    console.log(JSON.stringify(req.body, null, 2));
-    res.status(200);
+  let message: TelegramMessage;
+  try {
+    message = JSON.parse(req.body);
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+    return;
+  }
+  let response = message.message.text;
+  if (message.message.from.username != process.env.TELEGRAM_USERNAME) {
+    response = "You are not authorized to use this bot.";
+  }
+  let responseUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`;
+  await fetch(responseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: message.message.chat.id,
+      text: response,
+    }),
+  });
+  res.status(200);
 }
